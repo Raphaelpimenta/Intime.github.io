f3c7aeaaec46922b94f3103e5ad00a64ff5dbfe1
Raphael Pimenta <raphael.pimentagomes@hotmail.com>
2023-07-19 18:36:44 -0300

    commit inicial

---
+++ js/modules/animaNumeros.js
@@ -0,0 +1,35 @@
+export default function initanimaNumeros(){
+
+    function animaNumeros(){
+        const numeros = document.querySelectorAll('[data-numero]');
+
+        numeros.forEach((item) => {
+            const total = +item.innerHTML;
+            const incremento = Math.floor(total / 100);
+            let start = 0;
+
+            const timer = setInterval(() => {
+                start = start + incremento;
+                item.innerHTML = start;
+                if(start > total){
+                    item.innerHTML = total;
+                    clearInterval(timer)
+                }
+            }, 25 * Math.random())
+        })
+
+    }
+
+    function handleMutation(mutation){
+        if(mutation[0].target.classList.contains('ativo')){
+            alvo.disconnect();
+            animaNumeros()
+        }
+    }
+
+    const alvoTarget = document.querySelector('.cards');
+    const alvo = new MutationObserver(handleMutation);
+
+    alvo.observe(alvoTarget, {attributes: true})
+
+}
---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initMenuHamburguer from "./modules/menuHamburguer.js";
+import initscrollSuave from "./modules/scrollSuave.js";
+import initanimaNumeros from "./modules/animaNumeros.js";
+import initanimacaoScroll from "./modules/animaScroll.js";
+
+initMenuHamburguer()
+initscrollSuave();
+initanimaNumeros();
+initanimacaoScroll();
